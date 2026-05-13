6254f61c6be67edf0c4c1ad63f424535d7638f01
Dakshesh8090 <dakshesh.singhsherawat8090@gmail.com>
2026-05-13 14:20:41 +0530

    fixed CORS

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://documindai-hub.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
